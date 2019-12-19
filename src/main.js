f0da3e10dc1cf2866f4cec67578e4813db4b09e2
gongcheng <3167878601@qq.com>
2019-12-19 14:47:11 +0800

    我的vue_demo01本地仓库

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 直接导入plugins/element.js
+import './plugins/element.js'
 Vue.config.productionTip = false
 
 new Vue({
