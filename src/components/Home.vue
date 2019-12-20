6a8e593031ec9a05a5d63f5c03433b0175094b9d
gongcheng <3167878601@qq.com>
2019-12-20 21:17:44 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    logout() {
+      // 清除登录信息
+      window.sessionStorage.clear()
+      // 重定向到登录页面
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 // 直接导入plugins/element.js
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
