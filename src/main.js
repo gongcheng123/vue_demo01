e026dd857d2eac47ca3b2ed7b17ecd6f5e7df5ac
gongcheng <3167878601@qq.com>
2019-12-23 12:43:01 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://www.klxin.cn:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
