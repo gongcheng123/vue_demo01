814cd42e804cd387053d65a31f924c5f2247dae4
gongcheng <3167878601@qq.com>
2019-12-23 14:26:18 +0800

    完成用户列表开发

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -277,6 +277,7 @@ export default {
       })
     },
     async removeUserById(id) {
+      console.log(id)
       //  询问用户是否删除数据
       const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
         confirmButtonText: '确定',
